1fa6db5656652dd264c3087faa5451b66610515a
Pavly Adel <binofam222@gmail.com>
2025-01-13 19:04:20 +0200

    functionality of kayak page all

--- src/models/kayak.ts
+++ src/models/kayak.ts
@@ -12,13 +12,17 @@ export interface KayakResult {
   }
   
   export interface Result {
-    Id: number
-    CenterName: string
-    CenterAddress: string
-    CenterCity: string
-    CenterState: string,
-    MainImage: string,
-    place: Place[]
+    Id: number;
+    CenterName: string;
+    CenterAddress: string;
+    CenterCity: string;
+    CenterState: string;
+    Images: string | null; // Images can be null or a comma-separated string
+    Latitude?: number; // Optional, since it might not always exist
+    Longitude?: number; // Optional, since it might not always exist
+    StreetLatitude?: number;
+    StreetLongitude?: number;
+    place?: Place[]; // If `place` exists, define it as an array of Place
   }
   
   export interface Place {
