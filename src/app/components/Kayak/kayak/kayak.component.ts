3c3f37b3bb9c18c57ddcefef5117352ad09b7bc8
Andrew28girgis <andrewgirgis25@outlook.com>
2025-01-30 17:59:51 +0200

    +

--- src/app/components/Kayak/kayak/kayak.component.ts
+++ src/app/components/Kayak/kayak/kayak.component.ts
@@ -511,6 +511,7 @@ UnBindShoppingCenter(){
 
     this.modalService.open(this.galleryModal, { size: 'lg', centered: true });
   }
+  
   async viewOnMap(lat: number, lng: number) {
     this.mapViewOnePlacex = true;
 
@@ -539,6 +540,7 @@ UnBindShoppingCenter(){
       title: 'Location Marker',
     });
   }
+
   addMarkerToStreetView(panorama: any, lat: number, lng: number) {
     const svgPath =
       'M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z';
@@ -745,7 +747,7 @@ UnBindShoppingCenter(){
         result.CenterState.toLowerCase().includes(search)
     ); 
   }
-  
+
   getResult(): void {
     const body: any = {
       Name: 'GetResult',
