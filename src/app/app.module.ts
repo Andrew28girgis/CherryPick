09ab51437932bf6f70e856e906be80d0a4e8edd1
Paula Ashraf <paula.a.pham@gmail.com>
2025-03-09 12:54:52 +0200

    finish navbar refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -68,7 +68,6 @@ import { ManagePropertiesComponent } from './components/manage-properties/manage
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MainComponent } from './components/main/main.component';
 
 const msalConfig = {
   auth: {
@@ -124,7 +123,6 @@ const loginRequest = {
     NumberWithCommasPipe,
     ManagePropertiesComponent,
     DashboardComponent,
-    MainComponent,
   ],
   imports: [
     BrowserModule,
