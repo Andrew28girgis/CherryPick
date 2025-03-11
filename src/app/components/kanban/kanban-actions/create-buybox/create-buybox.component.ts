63e80298c19c689bf6664fb21b5e84b97c2da643
Paula Ashraf <paula.a.pham@gmail.com>
2025-03-11 13:45:54 +0200

    create kanban action to create buybox

---
+++ src/app/components/kanban/kanban-actions/create-buybox/create-buybox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-buybox',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-buybox.component.html',
+  styleUrl: './create-buybox.component.css'
+})
+export class CreateBuyboxComponent {
+
+}
