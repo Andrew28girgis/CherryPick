f6dad6ed9e903967f9a57061bb58481a6b9699ed
Abdelrhman Adel <abdelrhmanadel2462005@gmail.com>
2025-03-12 11:32:10 +0200

    Remove unused Kanban-related components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,6 @@ import { LandingComponent } from './components/landing/landing.component';
 import { SummeryComponent } from './components/summery/summery.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { KanbanComponent } from './components/kanban/kanban.component';
-import { DetailsComponent } from './components/kanban/details/details.component';
-import { StakeHolderComponent } from './components/kanban/stake-holders/stake-holders.component';
-import { TasksComponent } from './components/kanban/tasks/tasks.component';
-import { PropertiesComponent } from './components/kanban/properties/properties.component';
-import { PropertiesspilitviewComponent } from './components/kanban/properties/widgets/propertiesspilitview/propertiesspilitview.component';
-import { PropertiesgridviewComponentComponent } from './components/kanban/properties/widgets/propertiesgridview-component/propertiesgridview-component.component';
-import { CommunicationComponent } from './components/kanban/communication/communication.component';
 import { TermsComponent } from './components/terms/terms.component';
 import { ManagePropertiesComponent } from './components/manage-properties/manage-properties.component';
 import { TenantComponent } from './components/tenant/tenant.component';
@@ -52,22 +45,6 @@ const routes: Routes = [
   {
     path: 'Kanban/:id',
     component: KanbanComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'Done',
-    component: DetailsComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'stake-holders',
-    component: StakeHolderComponent,
-    canActivate: [AuthGuardService],
-  },
-
-  {
-    path: 'tasks',
-    component: TasksComponent,
     canActivate: [AuthGuardService],
   },
   {
