c6fdc9140338329a9c78ee73b2dbd8a2d61783fc
Pavly Adel <binofam222@gmail.com>
2025-03-19 11:19:51 +0200

    notificationn

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -15,13 +15,9 @@ export class NotificationsComponent implements OnInit {
   message!: string;
   createdDate!: string;
   dropdownVisible: boolean = false;
-  unreadCount: number = 0; 
-  readCount: number = 0; 
-  constructor(
-    private PlacesService: PlacesService,
-    private eRef: ElementRef
-
-  ) {}
+  unreadCount: number = 0;
+  readCount: number = 0;
+  constructor(private PlacesService: PlacesService, private eRef: ElementRef) {}
   ngOnInit(): void {
     const storedContactId = localStorage.getItem('contactId');
     if (storedContactId) {
@@ -31,27 +27,31 @@ export class NotificationsComponent implements OnInit {
   }
   toggleDropdown(): void {
     this.dropdownVisible = !this.dropdownVisible;
-    if (this.dropdownVisible) {
-    }
   }
 
   GetUserNotifications(): void {
-  
     const body: any = {
       Name: 'GetUserNotifications',
       Params: {
         ContactId: this.ContactId,
       },
     };
-  
+
     this.PlacesService.GenericAPI(body).subscribe({
       next: (res: any) => {
         this.notificationsArray = res.json || [];
-          this.notificationsArray.sort((a, b) => {
-          return new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime();
+        this.notificationsArray.sort((a, b) => {
+          return (
+            new Date(b.createdDate).getTime() -
+            new Date(a.createdDate).getTime()
+          );
         });
-     this.readCount = this.notificationsArray.filter(notification => notification.isRead).length;
-     this.unreadCount = this.notificationsArray.filter(notification => !notification.isRead).length;
+        this.readCount = this.notificationsArray.filter(
+          (notification) => notification.isRead
+        ).length;
+        this.unreadCount = this.notificationsArray.filter(
+          (notification) => !notification.isRead
+        ).length;
         if (this.notificationsArray.length > 0) {
           this.message = this.notificationsArray[0].message;
           this.createdDate = this.notificationsArray[0].createdDate;
@@ -59,12 +59,10 @@ export class NotificationsComponent implements OnInit {
           this.message = '';
           this.createdDate = '';
         }
-  
       },
-
     });
   }
-  
+
   updateNotification(notification: any): void {
     console.log('Notification ID selected:', notification.id);
 
@@ -83,24 +81,13 @@ export class NotificationsComponent implements OnInit {
       next: (res: any) => {
         console.log('UpdateNotification response:', res);
       },
-      
-      error: (err: any) => {
-        console.error('Error updating notification:', err);
-      },
     });
     this.GetUserNotifications();
   }
-
-  isLast(notification: any): boolean {
-    return (
-      this.notificationsArray.indexOf(notification) ===
-      this.notificationsArray.length - 1
-    );
-  }
-    @HostListener('document:click', ['$event'])
-    handleOutsideClick(event: Event): void {
-      if (!this.eRef.nativeElement.contains(event.target)) {
-        this.dropdownVisible = false;
-      }
+  @HostListener('document:click', ['$event'])
+  handleOutsideClick(event: Event): void {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.dropdownVisible = false;
     }
+  }
 }
