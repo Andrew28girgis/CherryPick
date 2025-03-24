bc7381e53757bdaad7dbdb50aee55dd4ca2e3e41
Andrew28girgis <andrewgirgis28@gmail.com>
2025-03-24 15:35:44 +0200

    ++

--- src/app/features/kayak-home/emily-stages/emily-contact-email/emily-contact-email.component.ts
+++ src/app/features/kayak-home/emily-stages/emily-contact-email/emily-contact-email.component.ts
@@ -161,7 +161,7 @@ export class EmilyContactEmailComponent implements OnInit {
     const matchingEmails: any[] = this.BuyBoxEmails;
     
     // Filter emails that are related to the selected contact.
-    this.emailsSentContact = matchingEmails.filter((email: Mail) =>
+    this.emailsSentContact = matchingEmails?.filter((email: Mail) =>
       email?.ContactId === contact.ContactId ||
       (email?.MailsContacts && email.MailsContacts.some(
         (mc: MailsContact) => mc.MailContactId === contact.ContactId
@@ -264,7 +264,7 @@ export class EmilyContactEmailComponent implements OnInit {
   filterEmails(filterType: string): void {
     this.selectedFilter = filterType;
     // If no emails or contact selected, don't try to filter.
-    if (!this.selectedContact || this.emailsSentContact.length === 0) {
+    if (!this.selectedContact || this.emailsSentContact?.length === 0) {
       this.filteredEmails = [];
       this.selectedEmail = null;
       return;
