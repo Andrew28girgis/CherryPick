b3960facbaf6c0d3943dbf5f42bac6242928395b
Andrew28girgis <andrewgirgis28@gmail.com>
2025-03-24 10:06:02 +0200

    change config and DTO

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { getDomainConfig } from '../../config';
+import { getDomainConfig } from '../../shared/others/config';
 
 @Injectable({
   providedIn: 'root',
