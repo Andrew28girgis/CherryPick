e2bc55207e554d45f22cbeacaf3b7050fa33ca94
Andrew28girgis <andrewgirgis28@gmail.com>
2025-03-27 22:36:14 +0200

    ++

--- src/app/features/Submissions/logs.component.ts
+++ src/app/features/Submissions/logs.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlacesService } from 'src/app/core/services/places.service';
 import { ActivatedRoute } from '@angular/router';
-import { submission } from 'src/app/shared/models/submissions';
+import { IIPlace, submission } from 'src/app/shared/models/submissions';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { General } from 'src/app/shared/models/domain';
 @Component({
   selector: 'app-logs',
   standalone: true,
@@ -13,21 +13,21 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './logs.component.css',
 })
 export class SubmissionsComponent implements OnInit {
-  submissionsArray: submission[] = [];    
-  campaignId!:any;
+  submissionsArray: submission[] = [];
+  campaignId!: any;
+  General!: General;
 
-  constructor(private PlacesService: PlacesService,     private route: ActivatedRoute ,    private modalService: NgbModal,
-  
+  constructor(
+    private PlacesService: PlacesService,
+    private route: ActivatedRoute,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {
-    
     this.route.paramMap.subscribe((params) => {
       this.campaignId = params.get('campaignId');
       console.log(this.campaignId);
-      
     });
-
 
     this.fetchReceivedSubmissions();
   }
@@ -41,26 +41,99 @@ export class SubmissionsComponent implements OnInit {
     };
     this.PlacesService.GenericAPI(body).subscribe({
       next: (res: any) => {
-        this.submissionsArray = res.json || [];    
+        this.submissionsArray = res.json || [];
       },
     });
   }
 
+  places:IIPlace[] = [];
   openSubmissions(content: any, modalObject?: any): void {
     this.modalService.open(content, {
       ariaLabelledBy: 'modal-basic-title',
       size: 'lg',
       scrollable: true,
     });
+    this.places = modalObject.UserSubmissions[0].ShoppingCenters[0].Place;
+    // this.General.modalObject = modalObject;
+    console.log(`ww`);
+    console.log(this.places);
+    
+    
   }
 
-  getSumbussionCenterName(submission: submission) {
+  getSubmissionCenterName(submission: submission) {
     return submission.UserSubmissions[0].ShoppingCenters[0].CenterName;
   }
+  getSubmissionCenter(submission: submission) {
+    return submission.UserSubmissions[0].ShoppingCenters[0];
+  }
 
-  getSumbussionCenterAddress(submission: submission) {
-    return submission.UserSubmissions[0].ShoppingCenters[0].CenterCity + ', ' + submission.UserSubmissions[0].ShoppingCenters[0].CenterState;
+  getSubmissionCenterAddress(submission: submission) {
+    return (
+      submission.UserSubmissions[0].ShoppingCenters[0].CenterCity +
+      ', ' +
+      submission.UserSubmissions[0].ShoppingCenters[0].CenterState
+    );
+  }
+
+  getSubmissionSourceId(submission: submission) {
+    if (submission.UserSubmissions[0].SourceId == 1) {
+      return 'Landing Page';
+    } else if (submission.UserSubmissions[0].SourceId == 2) {
+      return 'Email';
+    } else {
+      return 'Internal';
+    }
+  }
+
+  acceptSubmission(submission: submission): void {
+    submission.UserSubmissions[0].StatusId = 1;
+    console.log(`Submission ${submission.Firstname} accepted!`);
   }
   
+  rejectSubmission(submission: submission): void {
+    submission.UserSubmissions[0].StatusId = -1;
+    console.log(`Submission ${submission.Firstname} rejected!`);
+  }
 
+
+  AcceptUserSubmission(submission: submission): void {
+    submission.UserSubmissions[0].StatusId = 1;
+  
+    const userSubmissionId = submission.UserSubmissions[0].Id; 
+    
+    const body: any = {
+      Name: 'AcceptUserSubmission',
+      Params: {
+        UserSubmissionId: userSubmissionId,
+      },
+    };
+  
+    this.PlacesService.GenericAPI(body).subscribe({
+      next: (res: any) => {
+        this.fetchReceivedSubmissions(); 
+      }
+    });
+  }
+  
+  RejectUsersubmission(submission: submission): void {
+    submission.UserSubmissions[0].StatusId = -1;
+  
+    const userSubmissionId = submission.UserSubmissions[0].Id;  
+    
+    const body: any = {
+      Name: 'RejectUsersubmission',
+      Params: {
+        UserSubmissionId: userSubmissionId, 
+      },
+    };
+  
+    this.PlacesService.GenericAPI(body).subscribe({
+      next: (res: any) => {
+        this.fetchReceivedSubmissions(); 
+      }
+    });
+  }
+  
+  
 }
--- src/app/shared/models/submissions.ts
+++ src/app/shared/models/submissions.ts
@@ -1,32 +1,33 @@
 export interface submission {
-    id: number
-    Firstname: string
-    Lastname: string
-    UserSubmissions: UserSubmission[]
-  }
-  
-  export interface UserSubmission {
-    Id: number
-    UserId: number
-    ShoppingCenterId: number
-    FileName: string
-    CreatedDate: string
-    CampaignId: number
-    ShoppingCenters: ShoppingCenter[]
-  }
-  
-  export interface ShoppingCenter {
-    id: number
-    CenterName: string
-    CenterCity: string
-    CenterState: string
-    Place: Place[]
-  }
-  
-  export interface Place {
-    BuildingSizeSf?: number
-    ForLeasePrice?: number
-    LeaseType?: string
-    Suite?: string
-  }
-  
+  id: number;
+  Firstname: string;
+  Lastname: string;
+  UserSubmissions: UserSubmission[];
+}
+
+export interface UserSubmission {
+  Id: number;
+  UserId: number;
+  ShoppingCenterId: number;
+  FileName: string;
+  SourceId: number;
+  StatusId: number;
+  CreatedDate: string;
+  CampaignId: number;
+  ShoppingCenters: ShoppingCenter[];
+}
+
+export interface ShoppingCenter {
+  id: number;
+  CenterName: string;
+  CenterCity: string;
+  CenterState: string;
+  Place: IIPlace[];
+}
+
+export interface IIPlace {
+  BuildingSizeSf?: number;
+  ForLeasePrice?: number;
+  LeaseType?: string;
+  Suite?: string;
+}
