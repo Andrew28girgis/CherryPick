93a5cdbac8c9ba6b8c9296172b9655ab3724af51
Andrew_Ashraf <andrewashraf3133@gmail.com>
2025-04-16 13:10:46 +0200

    ++

--- src/app/core/services/campaign-drawing.service.ts
+++ src/app/core/services/campaign-drawing.service.ts
@@ -893,10 +893,10 @@ export class CampaignDrawingService {
   
     const polygonObj = new google.maps.Polygon({
       paths: coordinates,
-      strokeColor: '#AA00FF',
+      strokeColor: '#4d65b4',
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: '#AA00FF',
+      fillColor: '#4d65b4',
       fillOpacity: 0.35,
       editable: false,
       draggable: false,
