7495c0eaf3b0213cf61dfe284bc821dd02e61803
Paula Ashraf <paula.a.pham@gmail.com>
2025-04-16 14:09:02 +0200

    setup basic contact brpker stepper

--- src/app/features/kayak-home/shopping-center-table/contact-broker/contact-broker.component.ts
+++ src/app/features/kayak-home/shopping-center-table/contact-broker/contact-broker.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Center } from 'src/app/shared/models/shoppingCenters';
 
 @Component({
@@ -7,5 +8,14 @@ import { Center } from 'src/app/shared/models/shoppingCenters';
   styleUrl: './contact-broker.component.css',
 })
 export class ContactBrokerComponent {
+  protected stepperSteps: number[] = Array.from({ length: 3 }, (_, i) => i + 1);
+  protected currentStep: number = 1;
+
   @Input() center!: Center;
+
+  constructor(private activeModal: NgbActiveModal) {}
+
+  closeActiveModal() {
+    this.activeModal.close();
+  }
 }
