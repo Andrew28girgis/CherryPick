bc7d02f2f25adc21252d9358869c189c8df4e120
Paula Ashraf <paula.a.pham@gmail.com>
2025-04-23 15:24:25 +0200

    update breadcrumb

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -3,15 +3,10 @@ import {
   BreadcrumbService,
   BreadcrumbItem,
 } from '../../../core/services/breadcrumb.service';
-import {
-  BreadcrumbService,
-  BreadcrumbItem,
-} from '../../../core/services/breadcrumb.service';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
-  styleUrl: './breadcrumb.component.css',
   styleUrl: './breadcrumb.component.css',
 })
 export class BreadcrumbComponent implements OnInit {
