74f6bc525763efb05bd8d12c524b6a7dba057946
Paula Ashraf <paula.a.pham@gmail.com>
2025-04-23 16:26:12 +0200

    update add campaign

--- src/app/core/services/campaign-drawing.service.ts
+++ src/app/core/services/campaign-drawing.service.ts
@@ -23,6 +23,7 @@ export class CampaignDrawingService {
     polygon: google.maps.drawing.OverlayType.POLYGON,
     circle: google.maps.drawing.OverlayType.CIRCLE,
   };
+  private drawingMode: string | null = null;
   private explorePolygons: IMapShape[] = [];
   private markers: { polygonId: number; marker: google.maps.Marker }[] = [];
 
@@ -78,7 +79,6 @@ export class CampaignDrawingService {
               polygonArea
             );
           } else {
-            this.drawnPolygons.push({ shape: newPolygon });
             this.showShapeNameOptions(map, newPolygon, false);
             this.addPolygonClickListener(map, newPolygon);
             this.addPolygonChangeListener(map, newPolygon);
@@ -100,7 +100,6 @@ export class CampaignDrawingService {
               circleAreaMiles
             );
           } else {
-            this.drawnCircles.push({ shape: newCircle });
             this.showShapeNameOptions(map, newCircle, true);
             this.addCircleClickListener(map, newCircle);
             this.addCircleChangeListener(map, newCircle);
@@ -264,60 +263,54 @@ export class CampaignDrawingService {
     shape: google.maps.Polygon | google.maps.Circle,
     isCircle: boolean
   ): void {
-    const position = this.getShapeTopPoint(shape, isCircle);
+    const position = isCircle
+      ? (shape as google.maps.Circle).getCenter()
+      : this.getPolygonCenter(shape as google.maps.Polygon);
 
     if (!position) return;
-
-    const offsetPosition = new google.maps.LatLng(
-      position.lat() + 0.003,
-      position.lng()
-    );
 
     const content = this.getShapeNameOptionsPopup();
 
     this.infoWindow.setContent(content);
-    this.infoWindow.setPosition(offsetPosition);
+    this.infoWindow.setPosition(position);
     this.infoWindow.open(map);
 
-    const deleteButtonInterval = setInterval(() => {
-      const deleteButton = document.getElementById('deleteShape');
-      if (deleteButton) {
-        clearInterval(deleteButtonInterval);
-        deleteButton.addEventListener('click', () => {
-          shape.setMap(null);
-          this.hidePopupContent();
-          let list = isCircle ? this.drawnCircles : this.drawnPolygons;
-          const index = list.findIndex((p) => p.shape === shape);
-          if (index !== -1) {
-            list.splice(index, 1);
-          }
-          this.onDrawingCancel.emit();
-        });
-      }
-    }, 100);
+    // Add a one-time click listener on the map to hide the shape and remove its listeners.
+    const mapClickListener = map.addListener('click', () => {
+      this.setDrawingMode(this.drawingMode);
+      // Hide the shape from the map.
+      shape.setMap(null);
+      // Remove all event listeners for this shape.
+      google.maps.event.clearInstanceListeners(shape);
+      // Close the infoWindow.
+      this.infoWindow.close();
+      // Remove this listener.
+      google.maps.event.removeListener(mapClickListener);
+    });
 
     setTimeout(() => {
-      google.maps.event.clearListeners(map, 'click');
-      const mapClickListener = map.addListener('click', () => {
-        const input = document.getElementById(
-          'shapeNameInput'
-        ) as HTMLInputElement;
+      const saveButton = document.getElementById('saveShapeNameBtn');
+      const input = document.getElementById(
+        'shapeNameInput'
+      ) as HTMLInputElement;
+
+      saveButton!.addEventListener('click', () => {
         const targetArray = isCircle ? this.drawnCircles : this.drawnPolygons;
+        targetArray.push({ shape: shape });
         const shapeEntry = targetArray.find((entry) => entry.shape === shape);
+        const name = input.value.trim();
 
         if (shapeEntry) {
-          shapeEntry.shape.set(
-            'label',
-            input?.value?.trim().length > 0 ? input.value.trim() : 'Shape'
-          );
+          shapeEntry.shape.set('label', name ? name : 'Shape');
         }
-
-        const event = isCircle ? this.onCircleCreated : this.onPolygonCreated;
-        event.emit(shapeEntry);
+        // Remove the one-time map click listener since the shape is saved.
+        google.maps.event.removeListener(mapClickListener);
 
         this.infoWindow.close();
-        this.addMapListener(map);
-        google.maps.event.removeListener(mapClickListener);
+
+        // emit created shape to the component for any operation
+        const event = isCircle ? this.onCircleCreated : this.onPolygonCreated;
+        event.emit(shapeEntry);
       });
     });
   }
@@ -421,47 +414,38 @@ export class CampaignDrawingService {
   }
 
   private getShapeNameOptionsPopup(): string {
-    return `
-          <div style="display: flex; align-items: center; gap: 0.5rem; padding: 0.5rem">
-            <input
-              id="shapeNameInput"
-              placeholder="Enter shape name"
-              type="text"
-              class="form-control"
-              style="border: none; background-color: transparent; box-shadow: none"
-            />
+    return `<div
+              style="
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 0.75rem;
+                padding: 1rem;
+              "
+            >
+              <div>To mode this shape double click on it then move.</div>
+              <input
+                id="shapeNameInput"
+                placeholder="Enter shape name"
+                type="text"
+                class="form-control"
+              />
 
-            <div style="cursor:pointer;" id="deleteShape">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
+              <button
+                id="saveShapeNameBtn"
+                style="
+                  background-color: black;
+                  color: white;
+                  border: none;
+                  padding: 5px 10px;
+                  cursor: pointer;
+                  border-radius: 5px;
+                  font-size: 14px;
+                "
               >
-                <path
-                  d="M21.0002 6.72998C20.9802 6.72998 20.9502 6.72998 20.9202 6.72998C15.6302 6.19998 10.3502 5.99998 5.12016 6.52998L3.08016 6.72998C2.66016 6.76998 2.29016 6.46998 2.25016 6.04998C2.21016 5.62998 2.51016 5.26998 2.92016 5.22998L4.96016 5.02998C10.2802 4.48998 15.6702 4.69998 21.0702 5.22998C21.4802 5.26998 21.7802 5.63998 21.7402 6.04998C21.7102 6.43998 21.3802 6.72998 21.0002 6.72998Z"
-                  fill="#292D32"
-                />
-                <path
-                  d="M8.49977 5.72C8.45977 5.72 8.41977 5.72 8.36977 5.71C7.96977 5.64 7.68977 5.25 7.75977 4.85L7.97977 3.54C8.13977 2.58 8.35977 1.25 10.6898 1.25H13.3098C15.6498 1.25 15.8698 2.63 16.0198 3.55L16.2398 4.85C16.3098 5.26 16.0298 5.65 15.6298 5.71C15.2198 5.78 14.8298 5.5 14.7698 5.1L14.5498 3.8C14.4098 2.93 14.3798 2.76 13.3198 2.76H10.6998C9.63977 2.76 9.61977 2.9 9.46977 3.79L9.23977 5.09C9.17977 5.46 8.85977 5.72 8.49977 5.72Z"
-                  fill="#292D32"
-                />
-                <path
-                  d="M15.2099 22.7501H8.7899C5.2999 22.7501 5.1599 20.8201 5.0499 19.2601L4.3999 9.19007C4.3699 8.78007 4.6899 8.42008 5.0999 8.39008C5.5199 8.37008 5.8699 8.68008 5.8999 9.09008L6.5499 19.1601C6.6599 20.6801 6.6999 21.2501 8.7899 21.2501H15.2099C17.3099 21.2501 17.3499 20.6801 17.4499 19.1601L18.0999 9.09008C18.1299 8.68008 18.4899 8.37008 18.8999 8.39008C19.3099 8.42008 19.6299 8.77007 19.5999 9.19007L18.9499 19.2601C18.8399 20.8201 18.6999 22.7501 15.2099 22.7501Z"
-                  fill="#292D32"
-                />
-                <path
-                  d="M13.6601 17.25H10.3301C9.92008 17.25 9.58008 16.91 9.58008 16.5C9.58008 16.09 9.92008 15.75 10.3301 15.75H13.6601C14.0701 15.75 14.4101 16.09 14.4101 16.5C14.4101 16.91 14.0701 17.25 13.6601 17.25Z"
-                  fill="#292D32"
-                />
-                <path
-                  d="M14.5 13.25H9.5C9.09 13.25 8.75 12.91 8.75 12.5C8.75 12.09 9.09 11.75 9.5 11.75H14.5C14.91 11.75 15.25 12.09 15.25 12.5C15.25 12.91 14.91 13.25 14.5 13.25Z"
-                  fill="#292D32"
-                />
-              </svg>
+                Save
+              </button>
             </div>
-          </div>
           `;
   }
 
@@ -590,8 +574,9 @@ export class CampaignDrawingService {
     this.addShapeCompletionListener(map);
   }
 
-  setDrawingMode(shape: string) {
-    this.drawingManager.setDrawingMode(this.modes[shape] || null);
+  setDrawingMode(shape: string | null) {
+    this.drawingMode = shape ?? null;
+    this.drawingManager.setDrawingMode(shape ? this.modes[shape] : null);
   }
 
   convertCircleToPolygon(
@@ -890,7 +875,7 @@ export class CampaignDrawingService {
     // const coordinates = polygon.geoJson?.geometry.coordinates[0].map((coord: number[]) => {
     //   return { lat: coord[1], lng: coord[0] };
     // });
-  
+
     const polygonObj = new google.maps.Polygon({
       paths: coordinates,
       strokeColor: '#4d65b4',
@@ -904,7 +889,7 @@ export class CampaignDrawingService {
     });
 
     polygonObj.setMap(map);
-      // debugger
+    // debugger
     // return polygonObj;
   }
   hidePolygon(polygonObj: google.maps.Polygon): void {
