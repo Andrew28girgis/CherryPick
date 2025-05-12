206a53c0136cbf45b6dec8076422e7277317f818
Andrew28girgis <andrewgirgis28@gmail.com>
2025-05-12 12:11:09 +0300

    +

---
+++ src/app/features/canvas/canvas-home/canvas-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './canvas-home.component.html',
+  styleUrl: './canvas-home.component.css',
+})
+export class CanvasHomeComponent {}
