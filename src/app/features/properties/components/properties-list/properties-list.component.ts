53623328bc71070210d9ebbbc24b486c1383ece2
Paula Ashraf <paula.a.pham@gmail.com>
2025-05-21 17:38:15 +0300

    create skelton effect

--- src/app/features/properties/components/properties-list/properties-list.component.ts
+++ src/app/features/properties/components/properties-list/properties-list.component.ts
@@ -7,8 +7,6 @@ import { PropertiesService } from '../../services/properties.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
-
-
 @Component({
   selector: 'app-properties-list',
   templateUrl: './properties-list.component.html',
@@ -16,13 +14,12 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class PropertiesListComponent implements OnInit {
   protected properties: IProperty[] = [];
+  protected dataLoaded: boolean = false;
 
   constructor(
     private placeService: PlacesService,
     private breadcrumbService: BreadcrumbService,
-    private spinner: NgxSpinnerService,
-    private propertiesService: PropertiesService,
-    
+    private propertiesService: PropertiesService
   ) {}
 
   ngOnInit(): void {
@@ -34,23 +31,21 @@ export class PropertiesListComponent implements OnInit {
     const properties = this.propertiesService.getProperties();
     if (properties.length) {
       this.properties = [...properties];
+      this.dataLoaded = true;
       return;
     }
 
-    this.spinner.show();
     const body = {
       Name: 'GetAllShoppingCenters',
       Params: {},
     };
 
     this.placeService.GenericAPI(body).subscribe((response) => {
-      this.spinner.hide();
+      this.dataLoaded = true;
       if (response.json && response.json.length) {
         this.properties = response.json;
         this.propertiesService.setProperties(response.json);
       }
     });
   }
-
-  
 }
