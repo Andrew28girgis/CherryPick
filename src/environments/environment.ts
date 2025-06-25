131a79ff95d1db9c3d7af8d9ad7afe8900341305
Paula Ashraf <paula.a.pham@gmail.com>
2025-06-25 17:47:43 +0300

    convert overview to configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // api: 'https://apibeta.cherrypick.com/api',
+  API_URL: 'https://apibeta.cherrypick.com/api',
   geoJsonsFilesPath: 'https://files.cherrypick.com/geojson',
   api: 'https://api.cherrypick.com/api',
   // api: 'https://localhost:5000/api',
