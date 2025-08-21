500cf3234fe1ad4eeabd5c201837e0ef529a830d
Andrew28girgis <andrewgirgis28@gmail.com>
2025-08-21 12:58:22 +0300

    +

---
+++ src/app/shared/components/ai-ui/ai-ui.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-ui',
+  standalone: true,
+  imports: [],
+  templateUrl: './ai-ui.component.html',
+  styleUrl: './ai-ui.component.css'
+})
+export class AiUIComponent {
+
+}
