dd4fc09a623551cc87943b64d2dbb069c7a65365
Abdelrhman Adel <abdelrhmanadel2462005@gmail.com>
2025-09-03 20:08:05 +0300

    +

--- src/app/features/kayak-home/shopping-center-table/shopping-center-table.component.ts
+++ src/app/features/kayak-home/shopping-center-table/shopping-center-table.component.ts
@@ -52,13 +52,13 @@ export class ShoppingCenterTableComponent implements OnInit, OnDestroy {
   filteredCenters: any[] = [];
   filteredCampaigns?: ICampaign[];
   isMobile = false;
-  currentView = 4; // Change from 3 to 4
+  currentView = 3; // Change from 3 to 4
   isSocialView = false;
   isMapView = false;
   organizationName!: string;
   CampaignId!: any;
   OrgId!: any;
-  selectedOption = 4; // Change from 5 to 4
+  selectedOption = 3; // Change from 5 to 4
   view = false;
   StageId = 0;
   stages: Stage[] = [];
@@ -200,7 +200,7 @@ export class ShoppingCenterTableComponent implements OnInit, OnDestroy {
     );
 
     // Modify the view initialization logic
-    const savedView = 4;
+    const savedView = 3;
     // if (savedView === '1') {
     //   this.currentView = 4; // Change from 3 to 4
     //   localStorage.setItem('currentViewDashBord', '4');
