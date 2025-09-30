a692ab8925d54a5847e48855c0715b790396c280
Andrew28girgis <andrewgirgis28@gmail.com>
2025-09-30 11:55:42 +0300

    +

--- src/app/core/services/generic-map.service.ts
+++ src/app/core/services/generic-map.service.ts
@@ -9,7 +9,6 @@ declare const google: any;
   providedIn: 'root',
 })
 export class GenericMapService {
-  private placesService?: google.maps.places.PlacesService;
   private tempCircles: { id: any; circle: google.maps.Circle }[] = [];
 
   onMapBoundsChanged = new EventEmitter<IMapBounds>();
@@ -93,10 +92,7 @@ export class GenericMapService {
     map.setZoom(zoomLevel);
   }
 
-  loadGeoJsonFileOnMap(
-    map: google.maps.Map,
-    url: string
-  ): Promise<any> {
+  loadGeoJsonFileOnMap(map: google.maps.Map, url: string): Promise<any> {
     return new Promise((resolve, reject) => {
       map.data.loadGeoJson(url, null, (features) => {
         try {
@@ -119,7 +115,11 @@ export class GenericMapService {
             type.trim().length > 0 &&
             type.toLowerCase() == 'point'
           ) {
-            if (this.tempCircles.find((t) => t.id == features[0].getProperty('REGIONID'))) {
+            if (
+              this.tempCircles.find(
+                (t) => t.id == features[0].getProperty('REGIONID')
+              )
+            ) {
               const tempCircle = this.tempCircles.find(
                 (t) => t.id == features[0].getProperty('REGIONID')
               );
@@ -135,9 +135,8 @@ export class GenericMapService {
             }
           }
 
-          
           // resolve with whatever ID we picked up (or undefined if none)
-          resolve(features[0].getProperty('REGIONID') ||undefined);
+          resolve(features[0].getProperty('REGIONID') || undefined);
         } catch (err) {
           reject(err);
         }
@@ -145,50 +144,30 @@ export class GenericMapService {
     });
   }
 
-  // loadGeoJsonFileOnMap(
-  //   map: google.maps.Map,
-  //   url: string
-  // ): number | string | undefined {
-  //   const zoomLevel = map.getZoom();
-  //   let featureId = undefined;
-  //   map.data.loadGeoJson(url, null, (features) => {
-  //     // run inside Angular’s zone if you’re binding to changes
-  //     const bounds = new google.maps.LatLngBounds();
-  //     features.forEach((f) => {
-  //       featureId = f.getId();
-
-  //       f.getGeometry()?.forEachLatLng((latlng) => bounds.extend(latlng));
-  //     });
-  //     map.fitBounds(bounds, 50);
-
-  //     google.maps.event.addListenerOnce(map, 'idle', () => {
-  //       if (zoomLevel != null) map.setZoom(zoomLevel + 2);
-  //     });
-  //   });
-
-  //   return featureId;
-  // }
-
   removeFeatureById(map: google.maps.Map, id: any): void {
     map.data.forEach((feature) => {
-    if (feature.getProperty('REGIONID') === id) { 
-      const type = feature.getGeometry()?.getType();
-      if (
-        feature.getGeometry() instanceof google.maps.Data.Point &&
-        type &&
-        type.trim().length > 0 &&
-        type.toLowerCase() == 'point'
-      ) {
-        if (this.tempCircles.find((t) => t.id == feature.getProperty('REGIONID'))) {
-          const tempCircle = this.tempCircles.find(
-            (t) => t.id == feature.getProperty('REGIONID')
-          );
-          tempCircle?.circle.setMap(null);
+      if (feature.getProperty('REGIONID') === id) {
+        const type = feature.getGeometry()?.getType();
+        if (
+          feature.getGeometry() instanceof google.maps.Data.Point &&
+          type &&
+          type.trim().length > 0 &&
+          type.toLowerCase() == 'point'
+        ) {
+          if (
+            this.tempCircles.find(
+              (t) => t.id == feature.getProperty('REGIONID')
+            )
+          ) {
+            const tempCircle = this.tempCircles.find(
+              (t) => t.id == feature.getProperty('REGIONID')
+            );
+            tempCircle?.circle.setMap(null);
+          }
         }
+        map.data.remove(feature);
       }
-      map.data.remove(feature);
-    }
-  });
+    });
   }
 
   getStatesInsideMapView(
@@ -291,125 +270,4 @@ export class GenericMapService {
     circle.setMap(map);
     return circle;
   }
-
-  // getCitiesInsideMapView(map: google.maps.Map, callback: (cities: IMapCity[]) => void): void {
-  //   const bounds = map.getBounds();
-
-  //   if (!bounds) {
-  //     callback([]); // If bounds are not available, return an empty array
-  //     return;
-  //   }
-
-  //   const ne = bounds.getNorthEast();
-  //   const sw = bounds.getSouthWest();
-  //   const latStep = (ne.lat() - sw.lat()) / 10; // Adjust grid size as needed
-  //   const lngStep = (ne.lng() - sw.lng()) / 10;
-
-  //   // Set to store unique cities (using placeId for uniqueness)
-  //   const uniqueCities: Set<string> = new Set();
-  //   const cityList: IMapCity[] = [];
-
-  //   // Use a counter to track the number of geocoding requests
-  //   let totalRequests = 0;
-  //   let completedRequests = 0;
-
-  //   // Loop through coordinates within the bounds to reverse geocode
-  //   for (let lat = sw.lat(); lat < ne.lat(); lat += latStep) {
-  //     for (let lng = sw.lng(); lng < ne.lng(); lng += lngStep) {
-  //       totalRequests++;
-  //       this.reverseGeocode(lat, lng, uniqueCities, cityList, () => {
-  //         completedRequests++;
-  //         // If all geocoding requests are complete, invoke the callback with the result
-  //         if (completedRequests === totalRequests) {
-  //           callback(cityList); // Return the cityList through the callback
-  //         }
-  //       });
-  //     }
-  //   }
-
-  //   // If no requests were made (empty bounds), invoke the callback with an empty array
-  //   if (totalRequests === 0) {
-  //     callback([]);
-  //   }
-  // }
-
-  // reverseGeocode(lat: number, lng: number, uniqueCities: Set<string>, cityList: IMapCity[], callback: () => void): void {
-  //   const geocoder = new google.maps.Geocoder();
-  //   geocoder.geocode({ location: { lat, lng } }, (results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) => {
-  //     if (status === 'OK' && results[0]) {
-  //       const city = results.find((result: google.maps.GeocoderResult) =>
-  //         result.types.includes('locality')
-  //       );
-  //       if (city) {
-
-  //         // Create IMapCity object
-  //         const mapCity: IMapCity = {
-  //           name: city.formatted_address,
-  //           lat: city.geometry.location.lat(),
-  //           lng: city.geometry.location.lng(),
-  //           placeId: city.place_id,
-  //         };
-
-  //         // Check if the placeId is unique and add it to the list
-  //         if (!uniqueCities.has(mapCity.placeId)) {
-  //           uniqueCities.add(mapCity.placeId);
-  //           cityList.push(mapCity);
-  //         }
-  //       }
-  //     }
-  //     callback(); // Call the callback after processing this request
-  //   });
-  // }
-
-  // getCitiesInsideMapView(
-  //   map: google.maps.Map,
-  //   callback: (cities: IMapCity[]) => void
-  // ): void {
-  //   if (!map) {
-  //     callback([]); // If no map, return empty array through callback
-  //     return;
-  //   }
-
-  //   if (this.placesService) {
-  //     this.placesService = undefined;
-  //   }
-  //   this.placesService = new google.maps.places.PlacesService(map);
-
-  //   if (this.placesService) {
-  //     const bounds = map.getBounds();
-
-  //     this.placesService.nearbySearch(
-  //       { bounds, type: 'locality' },
-  //       (results, status) => {
-  //         if (
-  //           status !== google.maps.places.PlacesServiceStatus.OK ||
-  //           !results
-  //         ) {
-  //           callback([]); // Call the callback with empty array if results are invalid
-  //           return;
-  //         }
-
-  //         const cities: IMapCity[] = results
-  //           .filter(
-  //             (place) =>
-  //               !!place.geometry &&
-  //               !!place.geometry.location &&
-  //               !!place.name &&
-  //               !!place.place_id
-  //           )
-  //           .map((place) => {
-  //             const loc = place.geometry!.location!;
-  //             return {
-  //               name: place.name!,
-  //               lat: loc.lat(),
-  //               lng: loc.lng(),
-  //               placeId: place.place_id!,
-  //             };
-  //           });
-
-  //         callback(cities); // Call the callback with the filtered cities
-  //       }
-  //     );
-  //   }
-  // }
 }
