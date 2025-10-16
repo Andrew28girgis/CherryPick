ae9ae58b535cc5b1c07f256240c6f761fed3bb5b
Abdelrhman Adel <abdelrhmanadel2462005@gmail.com>
2025-10-16 18:01:02 +0300

    +

--- src/app/shared/models/shopping.ts
+++ src/app/shared/models/shopping.ts
@@ -1,49 +1,51 @@
 export interface ShoppingCenter {
-  scId: number
-  centerName: string
-  centerAddress: string
-  centerCity: string
-  centerState: string
-  mainImage: string
-  latitude: number
-  longitude: number
-  docsLink: string
-  centerType: string
-  category: string
-  lastUpdateDate: Date
-  source: string
-  campaignId: number
-  managerORG: string
-  managerORGLogoURL: string
-  shoppingCenter: ShoppingCenterarr
+  scId: number;
+  centerName: string;
+  centerAddress: string;
+  centerCity: string;
+  centerState: string;
+  mainImage: string;
+  latitude: number;
+  longitude: number;
+  docsLink: string;
+  centerType: string;
+  category: string;
+  lastUpdateDate: Date;
+  source: string;
+  campaignId: number;
+  managerORG: string;
+  managerORGLogoURL: string;
+  shoppingCenter: ShoppingCenterarr;
   isShared?: boolean;
   isDropdownOpen?: boolean;
   buildingSizeSf?: number;
   forSalePrice?: number;
   lastForSalePrice?: number;
-  organizationName:string
+  organizationName: string;
   forLeasePrice?: number;
+  centerZibCode: any;
+  centerCounty: any;
 }
 
 export interface ShoppingCenterarr {
-  managerOrganization: ManagerOrganization[]
-  places: Place[]
+  managerOrganization: ManagerOrganization[];
+  places: Place[];
 }
 
 export interface ManagerOrganization {
-  id: number
-  name: string
-  contactId: number
-  firstname: string
-  lastName: string
-  email: string
+  id: number;
+  name: string;
+  contactId: number;
+  firstname: string;
+  lastName: string;
+  email: string;
 }
 
 export interface Place {
-  id: number
-  buildingSizeSf: number
-  secondaryType: string
-  suite: string
-  price: number;         // lowercase
-  leaseType: string
+  id: number;
+  buildingSizeSf: number;
+  secondaryType: string;
+  suite: string;
+  price: number; // lowercase
+  leaseType: string;
 }
