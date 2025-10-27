5610c17f4b84f6cc1e73037b47728b0b9ddffa47
Andrew28girgis <andrewgirgis28@gmail.com>
2025-10-27 15:36:55 +0300

    +

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   API_URL: 'https://apibeta.cherrypick.com/api',
   geoJsonsFilesPath: 'https://files.cherrypick.com/geojson',
-  api: 'https://valorcp.cherrypick.com/api',
-  // api: 'https://emily.app/api',
+  // api: 'https://valorcp.cherrypick.com/api',
+  api: 'https://emily.app/api',
 };
